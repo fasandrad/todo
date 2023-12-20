51152e64a453983e85e93f937a2cc6404c664fea
Felipe Casseb <felipe.casseb@hotmail.com>
2023-12-19 22:31:48 -0300

    UI login_screen.dart

---
+++ lib/screens/signup_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class SignupScreen extends StatefulWidget {
+  const SignupScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SignupScreen> createState() => _SignupScreenState();
+}
+
+class _SignupScreenState extends State<SignupScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Nova conta"),
+        centerTitle: true,
+        backgroundColor: Colors.indigo,
+      ),
+    );
+  }
+}
