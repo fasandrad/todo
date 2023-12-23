72e662ef1f920de19e028a2b3cf5560c294796c1
Felipe Andrade <fasandrad@gmail.com>
2023-12-23 12:46:51 -0300

    feat: update signup_screen

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:todo/screens/login_screen.dart';
 
 class SignupScreen extends StatefulWidget {
   const SignupScreen({Key? key}) : super(key: key);
@@ -8,13 +10,57 @@ class SignupScreen extends StatefulWidget {
 }
 
 class _SignupScreenState extends State<SignupScreen> {
+
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Nova conta"),
+        title: Text("Nova Conta", style: TextStyle(fontSize: 32, fontWeight: FontWeight.w700),),
         centerTitle: true,
-        backgroundColor: Colors.indigo,
+        backgroundColor: Colors.white,
+      ),
+      body:SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                controller: emailController,
+                keyboardType: TextInputType.emailAddress,
+                textInputAction: TextInputAction.next,
+                decoration: InputDecoration(
+                  labelText: "E-mail",
+                ),
+              ),
+              TextField(
+                controller: passwordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: "Senha",
+                ),
+              ),
+              ElevatedButton(
+                onPressed: (){
+                  debugPrint("Email: ${emailController.text}, Senha: ${passwordController.text}");
+                },
+                child: Text("Salvar"),
+              ),
+              Text("Já possui conta?", style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),),
+              TextButton(
+                onPressed: (){
+                  Navigator.of(context).pop(
+                    MaterialPageRoute(builder: (context) => LoginScreen())
+                  );
+                },
+              child: Text("Login", style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
